5bfd74dfc090e8927000edbbba61c9d27fce1f4f
zckirkland <zachariahcaleb@gmail.com>
2021-07-05 14:22:04 -0500

    Add var-pokemonList and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
-alert('Hello World');
-
-let favoriteFood = 'Jalapeno Poppers';
-document.write(favoriteFood);
+var pokemonList = [
+    {
+        name: 'Charizard',
+        height: '1.7',
+        type: ['fire', 'flying'],
+        attack: '84',
+    },
+    {
+        name: 'Fearow',
+        height: '1.2',
+        type: ['flying', 'normal'],
+        attack: '90',
+    },
+    {
+        name: 'Ninetales',
+        height: '1.1',
+        type: 'fire',
+        attack: '76',
+    },
+    {
+        name: 'Tentacruel',
+        height: '1.6',
+        type: ['water', 'poison'],
+        attack: '70',
+    },
+];
